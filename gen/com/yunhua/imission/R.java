1594f1f7e7563cd0b96b3c57d532576bb051f439
floydzhang1984 <tech_universe@163.com>
2015-03-03 16:29:20 +0800

    添加底部导航栏圆点图标

--- gen/com/yunhua/imission/R.java
+++ gen/com/yunhua/imission/R.java
@@ -26,9 +26,15 @@ public final class R {
         public static final int guidepage_2=0x7f020001;
         public static final int guidepage_3=0x7f020002;
         public static final int ic_launcher=0x7f020003;
+        public static final int login_point=0x7f020004;
+        public static final int login_point_selected=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080005;
+        public static final int guidepoint1=0x7f080002;
+        public static final int guidepoint2=0x7f080003;
+        public static final int guidepoint3=0x7f080004;
+        public static final int ll=0x7f080001;
         public static final int viewpager=0x7f080000;
     }
     public static final class layout {
--- src/com/yunhua/imission/GuidingPage.java
+++ src/com/yunhua/imission/GuidingPage.java
@@ -6,8 +6,10 @@ import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 
 /*
  * 为每幅图片创建View并存放到ViewArray中
@@ -15,11 +17,14 @@ import android.view.View;
  * 使用vp.setAdapter方法绑定Adapter
  */
 
-public class GuidingPage extends Activity {
+public class GuidingPage extends Activity implements OnPageChangeListener {
 	
 	private ViewPager vp;
 	private ViewPagerAdapter vpAdapter;
 	private List<View> views;
+	private ImageView[] dots;
+	private int[] ids = { R.id.guidepoint1, R.id.guidepoint2, R.id.guidepoint3 };
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class GuidingPage extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.guidingpage);
 		initViews();
+		initDots();
 	}
 	
 	private void initViews() {
@@ -40,6 +46,37 @@ public class GuidingPage extends Activity {
 		vpAdapter = new ViewPagerAdapter(views, this);
 		vp = (ViewPager) findViewById(R.id.viewpager);
 		vp.setAdapter(vpAdapter);
+		vp.setOnPageChangeListener(this);
+	}
+	
+	private void initDots() {
+		dots = new ImageView[views.size()];
+		for( int i = 0; i < views.size(); i++) {
+			dots[i] = (ImageView) findViewById(ids[i]);
+		}
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPageSelected(int arg0) {
+		for(int i = 0; i < ids.length; i++) {
+			if( arg0 == i){
+				dots[i].setImageResource(R.drawable.login_point_selected);
+			} else {
+				dots[i].setImageResource(R.drawable.login_point);
+			}
+		}
 		
 	}
 }
